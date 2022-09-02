53a57bdd0e7928bce2be4fd7d8035dc78ad41a3a
Jhonatan-Azevedo <dev.azevedo@outlook.com>
2022-09-02 14:45:32 -0300

    ADD: init vue with typescript

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,40 @@
+<template>
+  <header>
+    <h1>
+      <span> To-do-List </span>
+      <span class="icon">
+        <i class="fa fa-list-check"></i>
+      </span>
+    </h1>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "BarraLateral",
+});
+</script>
+
+<style scoped>
+header {
+  background-color: #00d7f38e;
+  width: 100%;
+  height: 100vh;
+  text-align: center;
+  padding: 5rem;
+}
+
+header h1 {
+  color: #fff;
+  font-weight: bold;
+  font-size: 1.5rem;
+}
+
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="box">
+    <div class="columns">
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="Formulário para criação de uma nova tarefa"
+      >
+        <input
+          type="text"
+          class="input"
+          placeholder="Qual tarefa você deseja iniciar ?"
+        />
+      </div>
+
+      <div class="column">
+        <div
+          class="is-flex is-align-items-center is-justify-content-space-between"
+        >
+          <section>
+            <strong>00:00:00</strong>
+          </section>
+          <button class="button">
+            <span class="icon">
+              <i class="fa fa-play"></i>
+            </span>
+            <span>play</span>
+          </button>
+          <button class="button">
+            <span class="icon">
+              <i class="fa fa-stop"></i>
+            </span>
+            <span>stop</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Formulario",
+});
+</script>
+
+<style scoped>
+</style>
