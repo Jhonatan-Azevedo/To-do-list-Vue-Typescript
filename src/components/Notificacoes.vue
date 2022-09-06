2e1b0cd1ce6c27beeb885f944827e7db0482d63b
Jhonatan Azevedo <dev.azevedo@outlook.com>
2022-09-06 10:37:09 -0300

    ADD: adicionando componente para notificacoes

---
+++ src/components/Notificacoes.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="notificacoes">
+    <article class="message is-success">
+      <div class="message-header">
+        <p>Success</p>
+        <button class="delete" aria-label="delete"></button>
+      </div>
+      <div class="message-body">Teste de sucesso</div>
+    </article>
+    <article class="message is-warning">
+      <div class="message-header">
+        <p>Success</p>
+        <button class="delete" aria-label="delete"></button>
+      </div>
+      <div class="message-body">Teste de sucesso</div>
+    </article>
+    <article class="message is-danger">
+      <div class="message-header">
+        <p>Success</p>
+        <button class="delete" aria-label="delete"></button>
+      </div>
+      <div class="message-body">Teste de sucesso</div>
+    </article>
+  </section>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Notificacoes",
+});
+</script>
+
+<style scoped>
+.notificacoes {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 300px;
+  padding: 8px;
+  z-index: 1000;
+}
+</style>
